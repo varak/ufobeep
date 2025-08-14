666c9453dc55b0c578721032a736d23d2dfbae12
varak <varak@yahoo.com>
2025-08-14 15:16:15 -0700

    Update to Beta v1.0.0-beta.4 and fix GitHub links
    - Increment app version to 1.0.0-beta.4+4 in pubspec.yaml
    - Update /app page to reflect beta version instead of alpha
    - Update iOS testing instructions to beta terminology
    - Fix GitHub repository URLs to correct varak/ufobeep
    - Update AppDownloadCTA with correct beta version info
    - Deploy new beta APK to production server via SCP
    - Update file sizes and version references throughout website

---
+++ app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,34 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audioplayers_darwin
+import device_info_plus
+import file_picker
+import file_selector_macos
+import firebase_core
+import firebase_messaging
+import gal
+import geolocator_apple
+import package_info_plus
+import path_provider_foundation
+import photo_manager
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
